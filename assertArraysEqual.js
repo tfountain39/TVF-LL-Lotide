6007913d38298a3f77f032b4b617fc06acc99ddc
Todd <todd.fountain39@gmail.com>
2023-09-25 23:45:12 -0500

    Push of refactored W2D1 Files

---
+++ assertArraysEqual.js
@@ -0,0 +1,10 @@
+const eqArrays = require(`./eqArrays`);
+
+const assertArraysEqual = function(array1, array2) {
+  const arraysAreEqual = eqArrays(array1, array2);
+  arraysAreEqual
+    ? console.log(`✅Assertion Passed: ${array1} and ${array2} are equal`)
+    : console.log(`🛑Assertion Failed: ${array1} and ${array2} are not equal`);
+};
+
+module.exports = assertArraysEqual;
--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArrayEqual = function(array1, array2) {
-  const arrayAreEqual = eqArrays(array1, array2);
-  arrayAreEqual
-    ? console.log(`✅Assertion Passed: ${array1} and ${array2} are equal`)
-    : console.log(`🛑Assertion Failed: ${array1} and ${array2} are not equal`);
-};
-
 const middle = function(array) {
   if (array.length <= 2) {
     return [];
@@ -33,5 +13,4 @@ const middle = function(array) {
   }
 };
 
-
-assertArrayEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5])
+module.exports = middle;
